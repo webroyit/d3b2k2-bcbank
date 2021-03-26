52844a619103aacbcdef3010f7c8dc21bf249f17
royweb <xytank@gmail.com>
2021-03-26 13:30:53 -0400

    Add a function to allow the user to deposit their ETH to the dbank contract

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Notes
 - `msg.sender` is a global variable inside of solidity
+- `msg.value` is the ether amount
 - `truffle compile` compiled the smart contract to code that the ethereum virtual machine (EVM) can read
 - 'abi' (abstract binary interface) is a json representation of the smart contract
 - WEI is like a penny for ethereum
+- Gas fee occur when you create transactions on the ethereum network
+- We have gas fee because it is used to prevent spam on the network and it help maintain the security of the blockchain
+- Event in solidity is used to keep track of certain activities happen on the blockchain 
--- contracts/dBank.sol
+++ contracts/dBank.sol
@@ -7,9 +7,37 @@ contract dBank {
     // Assign Token contract to variable
     Token private token;
 
+    // Add mappings
+    // Can be used for database
+    mapping(address => uint) public etherBalanceOf;
+    mapping(address => uint) public depositStart;
+    mapping(address => bool) public isDeposited;
+
+    //add events
+    event Deposit(address indexed user, uint etherAmount, uint timeStart);
+
     // Pass as constructor argument deployed Token contract
     constructor(Token _token) public {
         // Assign token deployed contract to variable
         token = _token;
+    }
+
+    function deposit() payable public {
+        // Check if msg.sender didn't already deposited funds
+        require(isDeposited[msg.sender] == false, "Error, deposit already active");
+        // Check if msg.value is >= than 0.01 ETH
+        require(msg.value >= 1e16, "Error, deposit must be >= 0.01 ETH");
+
+        // Increase msg.sender ether deposit balance
+        etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+        // Start msg.sender holding time
+        depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+
+        // Set msg.sender deposit status to true
+        // Activate deposit status
+        isDeposited[msg.sender] = true;
+
+        // Emit Deposit event
+        emit Deposit(msg.sender, msg.value, block.timestamp);
     }
 }
