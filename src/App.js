c7f8c021040f9a68effeff6bf93fd5fbaf1cc0e2
royweb <xytank@gmail.com>
2021-03-27 13:31:08 -0400

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
+import { Tabs, Tab } from 'react-bootstrap'
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>BC Bank</h1>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className='text-monospace'>
+        <nav className="navbar navbar-dark fixed-top bg-success flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="/"
+          >
+          <b>dBank</b>
+        </a>
+        </nav>
+        <div className="container-fluid mt-5 text-center">
+        <br></br>
+          <h1>{/*add welcome msg*/}</h1>
+          <h2>{/*add user address*/}</h2>
+          <br></br>
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                {/*add Tab deposit*/}
+                {/*add Tab withdraw*/}
+              </Tabs>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
