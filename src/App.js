1288b008354a95ef248c5e8154770d40453719f2
royweb <xytank@gmail.com>
2021-03-28 13:49:57 -0400

    Install web3 and use it to connect to the blockchain and load contracts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
 import { Tabs, Tab } from 'react-bootstrap'
+import dBank from './abis/dBank.json';
 import React, { Component } from 'react';
+import Token from './abis/Token.json';
+import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      web3: 'undefined',
+      account: '',
+      token: null,
+      dbank: null,
+      balance: 0,
+      dBankAddress: null
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadBlockchainData(this.props.dispatch)
+  }
+
+  async loadBlockchainData(dispatch) {
+    // Check if MetaMask exists
+    if(typeof window.ethereum !== 'undefined'){
+      // Get a copy of web3 from MetaMask
+      // MetaMask store a variable that connect to blockchain in the browser
+      // To connection to the blockchain
+      const web3 = new Web3(window.ethereum);
+
+      const netId = await web3.eth.net.getId();
+      const accounts = await web3.eth.getAccounts();
+      
+      // Load balance
+      if(typeof accounts[0] !=='undefined'){
+        const balance = await web3.eth.getBalance(accounts[0]);
+        this.setState({account: accounts[0], balance: balance, web3: web3});
+      }
+      else {
+        window.alert('Please login with MetaMask');
+      }
+
+      // Load contracts
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address);
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address);
+        const dBankAddress = dBank.networks[netId].address;
+        this.setState({token: token, dbank: dbank, dBankAddress: dBankAddress});
+      }
+      catch (e) {
+        console.log('Error', e);
+        window.alert('Contracts not deployed to the current network');
+      }
+    }
+    // If MetaMask not exists, push alert
+    else {
+      window.alert('Please install MetaMask');
+    }
+  }
 
   render() {
     return (
